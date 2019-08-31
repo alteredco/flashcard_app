8e77f83d1ec80758790ea885eb672238e3ea3332
Wendy <alteredco@gmail.com>
2019-08-31 23:55:11 +0200

    add post route and body-parser

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false}));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -12,8 +13,13 @@ app.get('/cards', (req, res) => {
   res.render('card', { prompt: "Who is buried in Grant's Tomb?"});
 });
 
-app.get('/sandbox', (req, res)=> {
-  res.render('sandbox', {firstName: "Colt", lastName: "Steele"})
+app.get('/hello', (req, res)=> {
+  res.render('hello')
+})
+
+app.post('/hello', (req, res)=> {
+  console.dir(req.body);
+  res.render('hello')
 })
 
 app.listen(3000, () => {
