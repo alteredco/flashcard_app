8cad2509decd4b66bb843131b12a5b3dc263b8b6
Wendy <alteredco@gmail.com>
2019-09-03 18:50:28 +0200

    change to goodbye route

--- app.js
+++ app.js
@@ -7,54 +7,11 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-// app.use((req,res,next)=> {
-//   console.log('hello')
-//   const err = new Error('I feel a disturbance in the force!')
-//   err.status = 500
-//   next(err)
-// });
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
 
-app.use((req,res,next) => {
-  console.log('world')
-  next()
-});
-
-app.get('/', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render('index', {name});
-  }else {
-    res.redirect('/hello')
-  }
-});
-
-app.get('/cards', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's Tomb?"});
-});
-
-app.get('/hello', (req, res)=> {
-  const name = req.cookies.username;
-  if(name) {
-    res.redirect('/')
-  }else {
-    res.render('hello')
-  }
-})
-
-app.get('/goodbye', (req, res)=>{
-     res.render('goodbye')
-})
-
-app.post('/hello', (req, res)=> {
-  res.cookie('username', req.body.username);
-  res.redirect('/')
-})
-
-app.post('/goodbye', (req, res)=>{
-  res.clearCookie('username');
-  res.redirect('/hello')
-})
-
+app.use(mainRoutes)
+app.use('/cards', cardRoutes)
 app.use((req,res,next)=> {
   const err = new Error('Not Found')
   err.status = 404
